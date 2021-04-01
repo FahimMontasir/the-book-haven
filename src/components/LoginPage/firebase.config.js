b9bc656470580d0e90c81a045d978c80c4b62111
FahimMontasir <76746363+FahimMontasir@users.noreply.github.com>
2021-04-01 13:04:08 +0600

    file removed
    firebase config file removed

--- src/components/LoginPage/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBVsjDHblY-sN-RqMcp5xJYd2BdIY25rfU",
-  authDomain: "the-book-haven.firebaseapp.com",
-  projectId: "the-book-haven",
-  storageBucket: "the-book-haven.appspot.com",
-  messagingSenderId: "141263138480",
-  appId: "1:141263138480:web:3b26bd6950682915f3d575"
-};
-export default firebaseConfig;
